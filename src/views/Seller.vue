51e3f138b98b038f51c2ef84c8e8a0bc33c411cb
helia_mh <helia.mhmdi@gmail.com>
2021-05-17 12:46:46 +0430

    call2

--- src/views/Seller.vue
+++ src/views/Seller.vue
@@ -43,19 +43,19 @@
     left: 25px;
     top: 30px;
 }">
-              <v-btn icon depressed style="color: rgb(119, 44, 232);
-    background: rgb(119, 44, 232);
-    float: left;">
-                <svg style="width:20px;height:20px;color: white" viewBox="0 0 24 24">
-                  <path fill="currentColor"
-                        d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z"/>
-                </svg>
-                <a style="color: white;font-size: 10px" href="callto://+02126403285">
+              <div style="float: left;">
+
+                <a style="color: white;font-size: 10px"  href="tel://+02126403285"
+                >
+                  <svg style="width:20px;height:20px;color: rgb(119, 44, 232)" viewBox="0 0 24 24">
+                    <path fill="currentColor"
+                          d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z"/>
+                  </svg>
                 </a>
-              </v-btn>
+              </div>
             </div>
           </v-row>
-
+<!--          href="//api.whatsapp.com/send?phone=09190914543&text=..."-->
         </div>
       </div>
       <router-view></router-view>
